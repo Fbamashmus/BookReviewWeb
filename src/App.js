4956170342ad3249deb7f94a7863618f9a7b0562
Fatima Bamashmoos <43080954+Fbamashmus@users.noreply.github.com>
2022-09-29 14:06:12 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './Component/Header';
+import Body from './Component/Body';
+import Footer from './Component/Footer';
+import Books from './Component/Books';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <Body />
+    <Books />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
